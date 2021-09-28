4ae9f74b9ab7f7480ac5578f0974555200e78cd6
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2021-09-28 23:10:35 +0200

    update

--- papi-gelato.py
+++ papi-gelato.py
@@ -1,10 +1,31 @@
+def smaakjes(bolletjeNummer):
+    smaak = input("Welke smaak wilt u voor bolletje nummer " + str(bolletjeNummer) + "? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?\n").lower()
+    if smaak == "a":
+        return "aardbei"
+    elif smaak == "c":
+        return "chocolade"
+    elif smaak == "m":
+        return "munt"
+    elif smaak == "v":
+        return "vanille"
+    else:
+        print("Sorry dat snap ik niet...")
+        smaakjes(bolletjeNummer)
+
 def stap1():
+    smaakjesLijst = []
     try:
         bolletjes = int(input("Hoeveel bolletjes wilt u?\n"))
         if bolletjes >= 1 and bolletjes <= 3:
+            for x in range(1, bolletjes+1):
+                smaak = smaakjes(x)
+                smaakjesLijst.append(smaak)
             stap2(str(bolletjes))
         elif bolletjes >= 4 and bolletjes <= 8:
             print("Dan krijgt u van mij een bakje met " + str(bolletjes) + " bolletjes")
+            for x in range(1, bolletjes+1):
+                smaak = smaakjes(x)
+                smaakjesLijst.append(smaak)
         else:
             print("Sorry, zulke grote bakken hebben we niet")
             stap1()
